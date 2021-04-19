2fcf6761954234757ab4574799dff7bf50b8db98
Iruiasaro <brunettirosario@gmail.com>
2021-04-19 09:43:51 +0200

    problemi di push  - manca animazione

---
+++ script.js
@@ -0,0 +1,68 @@
+//inserimento my form
+var myForm = document.getElementById("myform");
+
+myForm.addEventListener("submit", function (event) {
+
+//creazione delle variabili 
+var nomeUtenteElement = document.getElementById("nomeUtente");
+
+var kmDaFareElement = document.getElementById("kmDaFare");
+
+var ageElement = document.getElementById("age");
+
+event.preventDefault();
+var nomeUtente = nomeUtenteElement.value;
+
+var kmDaFare = kmDaFareElement.value;
+
+var age = ageElement.value;
+
+var scontistica = "";
+
+//condizione if per applicazione scontistica
+if (age == "maggiorenne") {
+  scontistica = "Nessuna scontistica applicata";
+} else if (age =="minorenne") {
+    scontistica = "Sconto minorenne -20%";
+  } else {
+    scontistica = "Sconto over65 -40% "
+  }
+
+//funzione per calcolo prezzo biglietto 
+function ticketPrice(myAge, kmDaFare) {
+  if (myAge == "over65") {
+    return ((0.21 * kmDaFare) - ((0.21 * kmDaFare) * 0.40)).toFixed(2)
+  }
+  if (myAge == "maggiorenne") {
+    return (0.21 * kmDaFare).toFixed(2)
+  }
+  if (myAge == "minorenne") {
+      return ((0.21 * kmDaFare) - ((0.21 * kmDaFare) * 0.20)).toFixed(2)
+  }
+}  
+
+//creazione funzione per calcolo numero random 9000 - 100000
+function randomCp() {
+  return Math.ceil(Math.random() * 10000) + 90000
+}
+
+//creazione funzione per numero random da 1 a 30
+function randomCarrozza() {
+  return Math.ceil(Math.random() * 30)
+}
+
+//stampa su html
+document.getElementById("passengerName").innerHTML = nomeUtente;
+
+document.getElementById("carrozzaName").innerHTML = randomCarrozza();
+
+document.getElementById("scontistica").innerHTML = scontistica;
+
+document.getElementById("CpCod").innerHTML = randomCp();
+
+document.getElementById("ticketPrice").innerHTML = (ticketPrice(age, kmDaFare));
+})
+
+//funzione di reset DA PERFEZIONARE
+myForm.addEventListener("reset", function () {
+})
